763baeb16975e2712aafc8dcedf8489c4cc857ff
syrobell <ado_adnen@hotmail.com>
2023-02-04 23:34:00 +0300

    portfolio page created

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let checkBox = document.getElementById("checkbox");
+const clip = document.querySelectorAll(".clip");
+
 
 checkBox.addEventListener("change", () => {
     if (checkBox.checked) {
@@ -18,4 +20,13 @@ function nodark() {
     document.body.classList.remove("darkmode")
     document.querySelector(".navLogo").classList.remove("darkLogo")
     document.querySelector(".footLogo").classList.remove("darkLogo")
+}
+
+for (let i = 0; i < clip.length; i++) {
+    clip[i].addEventListener("mouseenter", function(e) {
+        clip[i].play();
+    });
+    clip[i].addEventListener("mouseout", function(e) {
+        clip[i].pause();
+    })
 }
